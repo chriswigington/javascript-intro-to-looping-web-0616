f8d5c95607fa4fa9ba7c4dac73f6c2903d575a74
chriswigington <chriswigington@gmail.com>
2016-07-20 18:20:55 -0400

    Done.

---
+++ loops.js
@@ -0,0 +1,19 @@
+function forLoop(array) {
+  for (var i = 24; i >= 0; i--) {
+    array.push(`I am ${i} strange loop${i === 0 ? '' : 's'}.`)
+  }
+  return array
+}
+
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(n)
+    n--
+  } 
+  return "done"
+}
+
+function doWhileLoop(array) {
+  do {array.pop()} while (array.length > 0);
+  return array
+}
